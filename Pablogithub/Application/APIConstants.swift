dc37048e239031408bcf12f40311fbab1498f9a8
Pablo Roca <pablorocar@gmail.com>
2019-01-02 17:56:03 +0100

    fix personal token

--- Pablogithub/Application/APIConstants.swift
+++ Pablogithub/Application/APIConstants.swift
@@ -13,7 +13,7 @@ struct APIConstants {
     /// EPTrending - https://github-trending-api.now.sh/repositories?since=daily
     static let EPTrending = "ZbqcVvLexRPUV/ge5Q5jYXs0k62qujuqUtIcMs0oJHPkAYj9RTb5Ki/SiOdH9ahYcbIky0MWmwDqg3cjSpg1pw=="
 
-    /// token - cae60a338709915693f330b2a481cf938f44f5f0
+    /// personal token from account pabloapi - not disclosed
     static let token = "v6fOLFpmpZZY6S3AiCTz0qcHYS+1r36tZpG/WAG9bXl0XqZlG6OIjGalpLUk2xWO"
 
     /// EPUserInfo - /search/users?q=%@ in:login&amp;per_page=1
